31aa32f14a3b2d0b1d383f039bbf5bd0caffc008
Marsigli <marsigli1988@gmail.com>
2021-11-15 17:40:28 -0500

    render templates on list_snippets.html

--- snippet/views.py
+++ snippet/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def list_snippet(request):
-    snippet = Snippet.objects.all().order_by("title")
-    return render(request, "snippet/list_snippets.html", {"snippet": snippet})
+    snippets = Snippet.objects.all().order_by("title")
+    return render(request, "snippet/list_snippets.html", {"snippets": snippets})
 
 def add_snippet(request):
     if request.method == 'GET':
